3d50832a732deda57c0eeefc3bccf405617921e1
GabBilo <201731449@collegealma.ca>
2022-05-19 10:25:43 -0400

    Création eds fonctions pour identification

--- src/main/java/com/example/etc201731449/Controller.java
+++ src/main/java/com/example/etc201731449/Controller.java
@@ -9,8 +9,28 @@ public class Controller {
     public TextField nom;
     public DatePicker dob;
     public TextField email;
+    private final Model model = new Model();
+    public TextField identification;
 
 
     public void getPrenom(KeyEvent keyEvent) {
+        if (prenom.getText().length() <= 3){
+            model.modifIDPrenom(identification,prenom.getText());
+        }
+    }
+
+    public void getNom(KeyEvent keyEvent) {
+        if (nom.getText().length() <= 3){
+            model.modifIDNom(identification,nom.getText());
+        }
+    }
+
+    public void getDOB(KeyEvent keyEvent) {
+        if (dob.getValue() != null){
+            model.modifMonthYear(identification,dob.getValue());
+        }
+    }
+
+    public void getEmail(KeyEvent keyEvent) {
     }
 }
--- src/main/java/com/example/etc201731449/Model.java
+++ src/main/java/com/example/etc201731449/Model.java
@@ -1,4 +1,61 @@
 package com.example.etc201731449;
 
+import javafx.scene.control.TextField;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 public class Model {
+
+    private String firstName;
+    private String lastName;
+    private String month;
+    private String year;
+
+    public Model() {
+        this.firstName = "";
+        this.lastName = "";
+        this.month = "";
+        this.year = "";
+    }
+
+    public void modifIDPrenom(TextField identification, String prenom){
+        firstName = prenom.toLowerCase();
+        if (lastName.equals("") && month.equals("") && year.equals("")){
+            identification.setText(prenom.toLowerCase());
+        } else if(month.equals("") && year.equals("")){
+            identification.setText(lastName + "-" + prenom.toLowerCase());
+        } else {
+            identification.setText(lastName + "-" + prenom.toLowerCase() + "-" + month + "-" + year);
+        }
+    }
+
+    public void modifIDNom(TextField identification, String nom){
+        lastName = nom.toLowerCase();
+        if (firstName.equals("") && month.equals("") && year.equals("")){
+            identification.setText(nom.toLowerCase());
+        } else if(month.equals("") && year.equals("")){
+            identification.setText(nom.toLowerCase() + "-" + firstName);
+        } else {
+            identification.setText(nom.toLowerCase() + "-" + firstName + "-" + month + "-" + year);
+        }
+    }
+
+    public void modifMonthYear(TextField identification, LocalDate dob){
+        month = Integer.toString(dob.getMonthValue());
+        DateFormat df = new SimpleDateFormat("yy");
+        year = df.format(dob.getYear());
+
+
+        if (firstName.equals("") && lastName.equals("")){
+            identification.setText(month + "-" + year );
+        } else if(lastName.equals("")){
+            identification.setText(firstName + "-" + month + "-" + year);
+        } else if(firstName.equals("")) {
+            identification.setText(lastName + "-" + month + "-" + year);
+        } else {
+            identification.setText(lastName + "-" + firstName + "-" + month + "-" + year);
+        }
+    }
 }
